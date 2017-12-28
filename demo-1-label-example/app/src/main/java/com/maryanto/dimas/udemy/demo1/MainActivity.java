3b2999c9cc26a611ce7dacd5f249b6a3563bc9a4
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-12-28 23:30:50 +0700

    membuat project android sederhana

---
+++ demo-1-label-example/app/src/main/java/com/maryanto/dimas/udemy/demo1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.maryanto.dimas.udemy.demo1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clicked(View view){
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
